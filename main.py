abcc62900d05d903888df468d0a99d578c5b42fa
João Gabriel <128388946+JoaoGAO@users.noreply.github.com>
2023-05-16 21:57:13 +0000

    Resolução

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-num = int(input("Digite um número inteiro: "))
+numero = int(input("Digite um número inteiro: "))
 
-if num <= 0:
+if numero <= 0:
     print("Número inválido")
 else:
-    primo = True
-    i = 2
-    while i < num:
-        if num % i == 0:
-            primo = False
+    divisor = 2
+    eh_primo = True
+    
+    if numero == 1:
+        eh_primo = False
+    else:
+     while divisor < numero:
+        if numero % divisor == 0:
+            eh_primo = False
             break
-        i += 1
-    if primo:
+        divisor += 1
+
+    if eh_primo:
         print("Primo")
     else:
         print("Não primo")
